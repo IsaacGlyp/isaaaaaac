7ad186f990c7b8311febc943b791b14416678fa2
IsaacGlyp <isaacvandenaker@gmail.com>
2023-06-08 21:41:32 +0200

    color

--- main.js
+++ main.js
@@ -34,9 +34,9 @@ document.onselectionchange = function() {
     divStyler.style = "0px";
 
     if(divCount % 10 > 8){
-      divStyler.style.background = "rgb(123,100,175)";
+      divStyler.style.background = "rgb(200,200,200)";
       // mongoolStyler.style.color = "rgb(255,0,0)";
-      divStyler.style.color = "rgb(0,255,0)";
+      divStyler.style.color = "rgb(0,0,255)";
     }
     else{
       let r = Math.random() * 50;
